d11f64fafa830fcb4ccf9164c5d37abcf5908577
khayrul-atiq <khayrul.atiq@gmail.com>
2018-03-19 23:19:12 +0600

    tutorial link has been added

--- app/src/main/java/com/example/atiqa/locationtest001/MainActivity.java
+++ app/src/main/java/com/example/atiqa/locationtest001/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.atiqa.locationtest001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
+// tutorial link
+// https://www.youtube.com/watch?v=oihuhw0ayXA
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
